1019e605b669b38c5cb46b5592716176ea57a476
EMANI SAI KIRAN REDDY <112548987+SAIKIRANREDDY0014@users.noreply.github.com>
2026-03-10 21:55:49 +0530

    complted lists concept

--- lists/list1.py
+++ lists/list1.py
@@ -1,3 +1,24 @@
 list1 = ["sai","kiran","reddy"]
 print(list1)
+list2 = [1,344,22.22,"sai kiarn"]
+print(list2)
+print(list2[0])
+print(list2[1:4])
+list2.append("emani")
+# list2.sort()
+print(list2)
+
+list2.insert(5,"reddy")
+print(list2)
+
+print(list2.pop(2))
+list2.reverse()
+print(list2)
+
+list2.remove(344)
+print(list2)
+
+
+
+
 
