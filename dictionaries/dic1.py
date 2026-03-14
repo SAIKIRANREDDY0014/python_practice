808d413663af22c8bcb91e203873c26acc74b72a
EMANI SAI KIRAN REDDY <112548987+SAIKIRANREDDY0014@users.noreply.github.com>
2026-03-14 22:41:33 +0530

    dictionaries completed

---
+++ dictionaries/dic1.py
@@ -0,0 +1,31 @@
+dict  = {
+  "sai" : 33,
+  "ashok" : 99,
+  "mall" : "mother",
+  "lists" : [1,2,3,5]
+
+}
+
+print(dict,type(dict))
+print(dict["ashok"])
+print(dict["lists"])
+
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+dict.update({"sai":100})
+print(dict.items())
+dict.update({"praveen":"broinlaw"})
+print(dict.items())
+print(dict.get("ashok"))  #gives none if val not exists 
+
+print(dict.pop('lists'))
+print(dict.items())
+
+print(dict.popitem())
+print(dict.items())
+
+
+
+
+
