6486d6e4af8ee51d77f365afab9b0e653c04c1f9
EMANI SAI KIRAN REDDY <112548987+SAIKIRANREDDY0014@users.noreply.github.com>
2026-03-26 21:11:56 +0530

    completed functions

---
+++ Functions/func1.py
@@ -0,0 +1,30 @@
+def printnam(name):
+  print("Sai kiran reddy: ",name)
+
+def addnum(a,b):
+  return a+b
+
+def defpara(name,lastname="reddy"):
+  print(name+lastname)
+      
+# printnam("ashokreddy")  
+# a = addnum(55,6)
+# print(a)
+
+defpara("sai")
+defpara("sai","kiran")
+
+
+# factorial using recursion
+
+fact =0;
+def factorial(n):
+  if(n == 0 or n == 1):
+    return 1
+  fact = factorial(n-1) * n
+  return fact  
+
+f= factorial(5)
+print(f)
+
+
