eaf68131e19ed78561a9fc50cc414f3979a0c19c
EMANI SAI KIRAN REDDY <112548987+SAIKIRANREDDY0014@users.noreply.github.com>
2026-03-27 13:00:18 +0530

    doubt clearance

--- Functions/func1.py
+++ Functions/func1.py
@@ -11,20 +11,34 @@ def defpara(name,lastname="reddy"):
 # a = addnum(55,6)
 # print(a)
 
-defpara("sai")
-defpara("sai","kiran")
+# defpara("sai")
+# defpara("sai","kiran")
 
 
-# factorial using recursion
+# # factorial using recursion
 
-fact =0;
-def factorial(n):
-  if(n == 0 or n == 1):
+# fact =0;
+# def factorial(n):
+#   if(n == 0 or n == 1):
+#     return 1
+#   fact = factorial(n-1) * n
+#   return fact  
+
+# f= factorial(5)
+# print(f)
+
+
+# print("sai")
+# print("kiran", end ="")
+# print("reddy",end="")
+# print("emani")
+
+# sum of n natural numbers
+def sumofn(n):
+  if(n == 1):
     return 1
-  fact = factorial(n-1) * n
-  return fact  
+  sum = n + sumofn(n-1)
+  return sum;
 
-f= factorial(5)
-print(f)
-
-
+print(sumofn(6))
+  
